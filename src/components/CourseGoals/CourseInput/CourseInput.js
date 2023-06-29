17ed7e3b688a152cb02bbe80113eaa3854e6c55a
Kihyun Kim <rlgus3689@gmail.com>
2023-06-29 20:40:09 +1200

    styled-components with dynamic props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
@@ -11,12 +10,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${(props) => (props.$invalid ? "red" : "black")}
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${(props) => (props.$invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "salmon" : "transparent")}
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -27,14 +28,7 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-  &.invalid input {
-    border-color: red;
-    background: #ffd7d7;
-  }
-
-  &.invalid label {
-    color: red;
-  }
+  
 `;
 
 const CourseInput = (props) => {
@@ -59,7 +53,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && "invalid"}>
+      <FormControl $invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
