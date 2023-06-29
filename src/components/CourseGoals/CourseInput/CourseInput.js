f66921469fd0972a3321fa1fdce9d3014bbf1a17
Kihyun Kim <rlgus3689@gmail.com>
2023-06-29 21:01:12 +1200

    apply media query in Button

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,14 +10,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
-    color: ${(props) => (props.$invalid ? "red" : "black")}
+    color: ${(props) => (props.$invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
     border: 1px solid ${(props) => (props.$invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.invalid ? "salmon" : "transparent")}
+    background: ${(props) => (props.$invalid ? "#ffd7d7" : "transparent")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -28,7 +28,6 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-  
 `;
 
 const CourseInput = (props) => {
